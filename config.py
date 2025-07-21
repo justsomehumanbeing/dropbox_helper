825bb1c810ea46ef397a6b6517783dc391ffadec
LinoHaupt@nixosmachine <linojossfidel.haupt@uni-jena.de>
2025-07-21 22:37:55 +0200

    update the config.py for local need

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from pathlib import Path
 PASS_PREFIX = "services/uni/dropbox.com"
 
 # Dropbox link and local paths
-DROPBOX_REMOTE_PATH = "/path/in/dropbox"
-LOCAL_FILE_PATH = Path("./local_file")
+DROPBOX_REMOTE_PATH = "/TopomorphicComplexities/TopomorphicComplexities.tex"
+LOCAL_FILE_PATH = Path("../TopomorphicComplexities.tex")
 TIMESTAMP_LOG_PATH = Path("./.last_sync_time")
